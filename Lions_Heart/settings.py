4a373f2c07c3dc0dfa7798f1a8700122918f18bb
Tom <andypandy12345@yahoo.com>
2017-12-24 18:27:39 +0200

    Update.

--- Lions_Heart/settings.py
+++ Lions_Heart/settings.py
@@ -142,7 +142,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/home/django/Lions_Heart/media'
 
 CART_SESSION_ID = 'cart'
 
