99ad31824bf5069b710b87f8913ce9dfaf4f667d
Tom <andypandy12345@yahoo.com>
2017-12-24 15:45:50 +0200

    Update.

--- Lions_Heart/wsgi.py
+++ Lions_Heart/wsgi.py
@@ -7,10 +7,22 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
 
 from django.core.wsgi import get_wsgi_application
+
+
+sys.path.append('/lions_heart/lions_heart_env/Lions_Heart')
+
+sys.path.append('/lions_heart/lions_heart_env/Lib/site-packages')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Lions_Heart.settings")
 
 application = get_wsgi_application()
+
+
+
+
+
+
